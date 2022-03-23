001ea9df143002aa26f41b5b32f74361d5206b04
aznable12 <94750125+aznable12@users.noreply.github.com>
2022-03-24 06:45:03 +0700

    dialogue

--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
+
     private Queue <string> sentences;
 
     // Start is called before the first frame update
@@ -14,7 +20,9 @@ public class DialogueManager : MonoBehaviour
 
     public void StartDialogue (Dialogue dialogue)
     {
-        Debug.Log("Starting conversation with" + dialogue.name);
+        animator.SetBool("isOpen", true);
+
+        nameText.text = dialogue.name;
 
         sentences.Clear();
 
@@ -35,12 +43,12 @@ public class DialogueManager : MonoBehaviour
         }
 
         string sentence = sentences.Dequeue();
-        Debug.Log(sentence);
+        dialogueText.text = sentence;
     }
-
+     
     void EndDialogue()
     {
-
+        animator.SetBool("isOpen", false);
     }
     // Update is called once per frame
     void Update()
