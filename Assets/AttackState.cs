a53506234b564422bfcf112d2ab094a15e2443db
aznable12 <94750125+aznable12@users.noreply.github.com>
2022-03-26 16:03:39 +0700

    Update AttackState.cs

--- Assets/AttackState.cs
+++ Assets/AttackState.cs
@@ -14,8 +14,10 @@ public class AttackState : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+    
+        animator.transform.LookAt(player);
         float distance = Vector3.Distance(animator.transform.position, player.position);
-        if (distance < 20)
+        if (distance < 3.5f)
             animator.SetBool("isAttacking", false);
     }
 
