e1b7ed0fa7d0e581a3ffa7d72d6945c3a2bafb28
aznable12 <94750125+aznable12@users.noreply.github.com>
2022-05-16 18:30:34 +0700

    adding dynamic bone

--- Assets/DynamicBone/Scripts/DynamicBonePlaneCollider.cs
+++ Assets/DynamicBone/Scripts/DynamicBonePlaneCollider.cs
@@ -3,14 +3,11 @@
 [AddComponentMenu("Dynamic Bone/Dynamic Bone Plane Collider")]
 public class DynamicBonePlaneCollider : DynamicBoneColliderBase
 {
-    // prepare data
-    Plane m_Plane;
-
     void OnValidate()
     {
     }
 
-    public override void Prepare()
+    public override bool Collide(ref Vector3 particlePosition, float particleRadius)
     {
         Vector3 normal = Vector3.up;
         switch (m_Direction)
@@ -27,18 +24,14 @@ public class DynamicBonePlaneCollider : DynamicBoneColliderBase
         }
 
         Vector3 p = transform.TransformPoint(m_Center);
-        m_Plane.SetNormalAndPosition(normal, p);
-    }
-
-    public override bool Collide(ref Vector3 particlePosition, float particleRadius)
-    {
-        float d = m_Plane.GetDistanceToPoint(particlePosition);
+        Plane plane = new Plane(normal, p);
+        float d = plane.GetDistanceToPoint(particlePosition);
 
         if (m_Bound == Bound.Outside)
         {
             if (d < 0)
             {
-                particlePosition -= m_Plane.normal * d;
+                particlePosition -= normal * d;
                 return true;
             }
         }
@@ -46,29 +39,10 @@ public class DynamicBonePlaneCollider : DynamicBoneColliderBase
         {
             if (d > 0)
             {
-                particlePosition -= m_Plane.normal * d;
+                particlePosition -= normal * d;
                 return true;
             }
         }
-
-/*        // consider particleRadius
-        if (m_Bound == Bound.Outside)
-        {
-            if (d < particleRadius)
-            {
-                particlePosition += m_Plane.normal * (particleRadius - d);
-                return true;
-            }
-        }
-        else
-        {
-            if (d > -particleRadius)
-            {
-                particlePosition -= m_Plane.normal * (particleRadius + d);
-                return true;
-            }
-        }
-*/
         return false;
     }
 
@@ -77,18 +51,26 @@ public class DynamicBonePlaneCollider : DynamicBoneColliderBase
         if (!enabled)
             return;
 
-        Prepare();
-
         if (m_Bound == Bound.Outside)
-        {
             Gizmos.color = Color.yellow;
-        }
         else
-        {
             Gizmos.color = Color.magenta;
+
+        Vector3 normal = Vector3.up;
+        switch (m_Direction)
+        {
+            case Direction.X:
+                normal = transform.right;
+                break;
+            case Direction.Y:
+                normal = transform.up;
+                break;
+            case Direction.Z:
+                normal = transform.forward;
+                break;
         }
 
         Vector3 p = transform.TransformPoint(m_Center);
-        Gizmos.DrawLine(p, p + m_Plane.normal);
+        Gizmos.DrawLine(p, p + normal);
     }
 }
